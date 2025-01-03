cc02c3fae452a63383502148c614ea763238d609
Ignacio Arturo Varas Quinteros <ignacio.varasq@gmail.com>
2025-01-03 17:24:24 -0300

    base

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+//CSS
+import "@/app/ui/global.css";
+
+//FONTS
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
